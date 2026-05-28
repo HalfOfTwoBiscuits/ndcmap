257efdcdc6d450456168bdcf31d49c865e09a60c
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-05-28 13:45:33 +0100

    simplify query handling logic by catching exceptions explicitly rather than using a lambda

--- src/main/java/me/sirrahmas/ndcmap/queries/AbstractQuery.java
+++ src/main/java/me/sirrahmas/ndcmap/queries/AbstractQuery.java
@@ -1,6 +1,5 @@
 package me.sirrahmas.ndcmap.queries;
 
-import com.arcadedb.database.BasicDatabase.TransactionScope;
 import com.arcadedb.database.Database;
 import com.arcadedb.query.sql.executor.ResultSet;
 
@@ -16,17 +15,9 @@ public abstract class AbstractQuery {
     // If it throws an exception, the transaction will be rolled back
     // and the exception will be thrown again.
     // It may return a ResultSet to the caller, or return null.
-    abstract ResultSet doQuery(Database db);
+    abstract ResultSet doQuery(Database db) throws Exception;
 
-    // Return a lambda that passes the provided database object
-    // to the doQuery method, storing the result for later retrieval.
-    // The lambda retains the database object,
-    // so is capable of executing the query without any arguments,
-    // making it possible for `me.sirrahmas.ndcmap.QueryHandler`
-    // to use it in a transaction context.
-    // As in the ArcadeDB code, this lambda is referred to as
-    // the transaction scope.
-    TransactionScope getTransactionScope(Database db) {
-        return () -> {results = doQuery(db);};
+    void execute(Database db) throws Exception {
+        results = doQuery(db);
     }
 }
--- src/main/java/me/sirrahmas/ndcmap/queries/QueryHandler.java
+++ src/main/java/me/sirrahmas/ndcmap/queries/QueryHandler.java
@@ -1,6 +1,5 @@
 package me.sirrahmas.ndcmap.queries;
 
-import com.arcadedb.database.BasicDatabase.TransactionScope;
 import com.arcadedb.database.Database;
 import com.arcadedb.database.DatabaseFactory;
 import com.arcadedb.query.sql.executor.ResultSet;
@@ -9,16 +8,28 @@ import com.arcadedb.query.sql.executor.ResultSet;
 // The queries are represented by implementations of 
 // `me.sirrahmas.ndcmap.queries.AbstractQuery`.
 public class QueryHandler {
+
     private final DatabaseFactory dbf;
 
     public QueryHandler(DatabaseFactory databaseFactory) {
         dbf = databaseFactory;
     }
 
-    public ResultSet doQuery(AbstractQuery q) {
+    // Executes the query. Commits on success, on failure, rolls back and throws the exception again.
+    // Returns either the query result, or null for no result.
+    // NOTE: In a future project, maybe a better name than 'query' would be 'operation'?
+    public ResultSet doQuery(AbstractQuery q) throws Exception {
         try (Database db = dbf.open();) {
-            TransactionScope t = q.getTransactionScope(db);
-            db.transaction(t);
+            try {
+                db.begin();
+                q.execute(db);
+                db.commit();
+            } catch (Exception e) {
+                db.rollback();
+                throw e;
+            } finally {
+                db.close();
+            }
             return q.getResults();
         }
     }
