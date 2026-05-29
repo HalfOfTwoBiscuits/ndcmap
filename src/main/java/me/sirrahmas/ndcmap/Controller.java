d07d83c02ae8d363d221915182cfa0499ca38fb6
Umbertiscue <jaki@smileyfacebooks.co.uk>
2026-05-29 14:05:49 +0100

    add get endpoint for FetchAreaView

--- src/main/java/me/sirrahmas/ndcmap/Controller.java
+++ src/main/java/me/sirrahmas/ndcmap/Controller.java
@@ -3,6 +3,7 @@ package me.sirrahmas.ndcmap;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinFreemarker;
 import me.sirrahmas.ndcmap.queries.QueryHandler;
+import me.sirrahmas.ndcmap.views.FetchAreaView;
 import me.sirrahmas.ndcmap.views.MainPageView;
 
 // Controller class responsible for configuring URL routing for the app.
@@ -13,6 +14,7 @@ class Controller {
                 config.staticFiles.add("/static");
                 config.fileRenderer(new JavalinFreemarker());
                 config.routes.get("/", new MainPageView(qh));
+                config.routes.get("/getAreaFromPos/{x}/{y}", new FetchAreaView(qh));
             }
         );
     }
